ff7fbf46d85f847c54f41011bd3447512dbcfd0e
eleeeena1204 <65127920+eleeeena1204@users.noreply.github.com>
2023-09-16 18:38:29 +0200

    Último repaso

--- fun_mod.py
+++ fun_mod.py
@@ -18,24 +18,25 @@ def connect_db():
     return conn
 
 #Función /register o /registrar --------------------------------------------------------------------------------------------------------
-def register_user(message, bot):
+def register_user(message, bot, user, cont):
     '''Añadimos el user en la base de datos'''
-    if message.content_type == "text":
-        if message.text.startswith("/"):
-            conn = connect_db()
-            cursor = conn.cursor()
-            query = "SELECT * FROM usernames WHERE name like '@" + message.from_user.username + "' and chatId = " + str(message.chat.id)
-            cursor.execute(query)
-            results = cursor.fetchall()
-            cursor.close()
-            conn.close()
-            if len(results) == 0:
-                add_db("@" + message.from_user.username, message.chat.id)
-                bot.send_message(message.chat.id, "Usuario registrado.")
+    if message.from_user.id == user.id:
+        if message.content_type == "text":
+            check_swear_words(message, bot)
+            if message.text.startswith("/"):
+                conn = connect_db()
+                cursor = conn.cursor()
+                query = "SELECT * FROM usernames WHERE name like '@" + message.from_user.username + "' and chatId = " + str(message.chat.id)
+                cursor.execute(query)
+                results = cursor.fetchall()
+                cursor.close()
+                conn.close()
+                if len(results) == 0:
+                    add_db("@" + message.from_user.username, message.chat.id)
+                    bot.send_message(message.chat.id, "Usuario registrado.")
+                else:
+                    bot.send_message(message.chat.id, "Ya estabas registrado.") 
             else:
-                bot.send_message(message.chat.id, "Ya estabas registrado.") 
-        else:
-            if (bot.get_chat_member(message.chat.id, message.from_user.id).status in ["creator", "administrator"]) or (message.chat.id == MY_CHAT_ID):
                 if message.text.lower() != "exit" and message.text.lower() != "salir":
                     if message.text.startswith("@"):
                         conn = connect_db()
@@ -55,11 +56,18 @@ def register_user(message, bot):
                         bot.register_next_step_handler(msg, register_user, bot)
                 else:
                     bot.send_message(message.chat.id, "Se ha cancelado el registro del usuario.")
-            else:
-                msg = bot.send_message(message.chat.id, "Acción solo disponible para los administradores del grupo.\n\nPor favor, no interrumpas el proceso.\n\nEl administrador puede continuar o escribir 'salir' o 'exit' para cancelar.")
-                bot.register_next_step_handler(msg, register_user, bot)
+        else:
+            bot.send_message(message.chat.id, "Vaya, esto no es lo que esperaba...")
     else:
-        bot.send_message(message.chat.id, "Vaya, esto no es lo que esperaba...")
+        if cont == 9:
+            if user.username == None:
+                bot.send_message(message.chat.id, "Lo siento, " + user.first_name + ", no he podido finalizar el proceso debido a que has tardado muchos mensajes en responder, vuelve a intentarlo cuando quieras.")
+            else:
+                bot.send_message(message.chat.id, "Lo siento, @" + user.username + ", no he podido finalizar el proceso debido a que has tardado muchos mensajes en responder, vuelve a intentarlo cuando quieras.")
+        else:
+            if message.content_type == "text":
+                check_swear_words(message, bot)
+            bot.register_next_step_handler(message, register_user, bot, user, cont + 1)
 
 #Función para añadir usuarios a la base de datos en la bienvenida --------------------------------------------------------------------------------------------------------
 def add_db (member, chatId):
@@ -90,10 +98,11 @@ def delete_db (user, table):
     conn.close()
 
 #Función /warnings o /avisos --------------------------------------------------------------------------------------------------------
-def show_warnings(message, bot):
+def show_warnings(message, bot, user, cont):
     '''Mostramos los avisos que tiene un usuario'''
-    if message.content_type == 'text':
-        if (bot.get_chat_member(message.chat.id, message.from_user.id).status in ["creator", "administrator"]) or (message.chat.id == MY_CHAT_ID):
+    if message.from_user.id == user.id:
+        if message.content_type == "text":
+            check_swear_words(message, bot)
             if message.text.lower() != "exit" and message.text.lower() != "salir":
                 conn = connect_db()
                 cursor = conn.cursor()
@@ -135,10 +144,17 @@ def show_warnings(message, bot):
             else:
                 bot.send_message(message.chat.id, "Se ha cancelado la visualización de avisos.")
         else:
-            msg = bot.send_message(message.chat.id, "Acción solo disponible para los administradores del grupo.\n\nPor favor, no interrumpas el proceso.\n\nEl administrador puede continuar o escribir 'salir' o 'exit' para cancelar.")
-            bot.register_next_step_handler(msg, show_warnings, bot)
+            bot.send_message(message.chat.id, "Vaya, esto no es lo que esperaba...")
     else:
-        bot.send_message(message.chat.id, "Vaya, esto no es lo que esperaba...")
+        if cont == 9:
+            if user.username == None:
+                bot.send_message(message.chat.id, "Lo siento, " + user.first_name + ", no he podido finalizar el proceso debido a que has tardado muchos mensajes en responder, vuelve a intentarlo cuando quieras.")
+            else:
+                bot.send_message(message.chat.id, "Lo siento, @" + user.username + ", no he podido finalizar el proceso debido a que has tardado muchos mensajes en responder, vuelve a intentarlo cuando quieras.")
+        else:
+            if message.content_type == "text":
+                check_swear_words(message, bot)
+            bot.register_next_step_handler(message, show_warnings, bot, user, cont + 1)
 
 #Función para comprobar si en el mensaje hay palabrotas --------------------------------------------------------------------------------------------------------
 def check_swear_words(message, bot):
@@ -175,14 +191,14 @@ def check_swear_words(message, bot):
                 else:
                     if results[0][2] == 2:
                         #Baneado del grupo
-                        banEnd = datetime.now() + timedelta(minutes = 1) #ESTO SERÁN 24H
+                        banEnd = datetime.now() + timedelta(days = 1) 
                         query = "INSERT INTO bannedusers (id, name, banend, chatId) VALUES (" + str(message.from_user.id) + ", '" + user + "', '" + str(banEnd) + "', " + str(message.chat.id) + ")"
                         cursor.execute(query)
                         conn.commit()
                         query = "DELETE FROM warnedusers WHERE id = " + str(message.from_user.id)
                         cursor.execute(query)
                         conn.commit()
-                        bot.send_message(message.chat.id, "El usuario " + user + " ha sido baneado del grupo hasta " + str(banEnd) + ".")
+                        bot.send_message(message.chat.id, "El usuario " + user + " ha sido baneado del grupo hasta el día " + banEnd.strftime("%d/%m/%y") + " a las " + banEnd.strftime("%H:%M") + ".")
                         bot.ban_chat_member(message.chat.id, message.from_user.id, until_date = banEnd)
                     else:
                         query = "UPDATE warnedusers SET warnings = " + str(results[0][2]+1) + " WHERE id = " + str(results[0][0])
@@ -193,10 +209,11 @@ def check_swear_words(message, bot):
     conn.close()
 
 #Función /unban o /desbanear --------------------------------------------------------------------------------------------------------
-def unban_user(message, bot):
+def unban_user(message, bot, user, cont):
     '''Desbanear un usuario'''
-    if message.content_type == 'text':
-        if (bot.get_chat_member(message.chat.id, message.from_user.id).status in ["creator", "administrator"]) or (message.chat.id == MY_CHAT_ID):
+    if message.from_user.id == user.id:
+        if message.content_type == "text":
+            check_swear_words(message, bot)
             if message.text.lower() != "exit" and message.text.lower() != "salir":
                 conn = connect_db()
                 cursor = conn.cursor()
@@ -246,10 +263,17 @@ def unban_user(message, bot):
             else:
                 bot.send_message(message.chat.id, "Se ha cancelado el desbaneo del usuario.")
         else:
-            msg = bot.send_message(message.chat.id, "Acción solo disponible para los administradores del grupo.\n\nPor favor, no interrumpas el proceso.\n\nEl administrador puede continuar o escribir 'salir' o 'exit' para cancelar.")
-            bot.register_next_step_handler(msg, unban_user, bot)
+            bot.send_message(message.chat.id, "Vaya, esto no es lo que esperaba...")
     else:
-        bot.send_message(message.chat.id, "Vaya, esto no es lo que esperaba...")
+        if cont == 9:
+            if user.username == None:
+                bot.send_message(message.chat.id, "Lo siento, " + user.first_name + ", no he podido finalizar el proceso debido a que has tardado muchos mensajes en responder, vuelve a intentarlo cuando quieras.")
+            else:
+                bot.send_message(message.chat.id, "Lo siento, @" + user.username + ", no he podido finalizar el proceso debido a que has tardado muchos mensajes en responder, vuelve a intentarlo cuando quieras.")
+        else:
+            if message.content_type == "text":
+                check_swear_words(message, bot)
+            bot.register_next_step_handler(message, unban_user, bot, user, cont + 1)
 
 #Hacer listados con botones inline
 def show_pages(results, id, mid, bot):
